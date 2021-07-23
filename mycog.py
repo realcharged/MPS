ae2c543c0a268d9534d699742b71ca9765fa99fc
unknown <deaconjf0427@outlook.com>
2021-07-23 13:29:22 -0600

    added offline and online messages to ;mpsvr

--- mycog.py
+++ mycog.py
@@ -16,14 +16,22 @@ class MPS(commands.Cog):
         server = MinecraftServer.lookup("mps.modded.fun:25565")
         status = server.status()
         query = server.query()
-        embed = discord.Embed(
-            title=('This server is currently <online/offline>'
-                   '\n{0} players online'
-                   '\nPlayer List:'
-                   .format(status.players.online)),
-            description=('{0}'.format(", ".join(query.players.names)))
-        )
-        embed.set_author(name='MPSVR Status')
-        embed.set_footer(text='End of list')
+        latency = server.ping()
+        if latency > 1:
+            embed = discord.Embed(
+                title=('This server is currently Online'
+                       '\n{0} players online'
+                       '\nPlayer List:'
+                       .format(status.players.online)),
+                description=('{0}'.format(", ".join(query.players.names)))
+            )
+            embed.set_author(name='MPSVR Status ping {0}ms'.format(latency))
+            embed.set_footer(text='End of list')
 
-        await ctx.send(embed=embed)
+            await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(
+            title=('The Server is Offline'),
+            description=('Yell at <@166311283744964608>!')
+            )
+            await ctx.send(embed=embed)
