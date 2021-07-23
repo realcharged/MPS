e236b594fdbe594ab7095990fa603d4005b2c0cd
unknown <deaconjf0427@outlook.com>
2021-07-23 14:01:17 -0600

    quick fix

--- mycog.py
+++ mycog.py
@@ -13,25 +13,26 @@ class MPS(commands.Cog):
     @commands.command()
     async def mpsvr(self, ctx):
         """Displays server status."""
-        server = MinecraftServer.lookup("mps.modded.fun:25565")
-        status = server.status()
-        query = server.query()
-        latency = server.ping()
-        if latency > 1:
+        try:
+            server = MinecraftServer.lookup("mps.modded.fun:25565")
+            status = server.status()
+            query = server.query()
             embed = discord.Embed(
-                title=('This server is currently Online'
+                title=('MPSVR is currently Online'
                        '\n{0} players online'
                        '\nPlayer List:'
                        .format(status.players.online)),
                 description=('{0}'.format(", ".join(query.players.names)))
             )
-            embed.set_author(name='MPSVR Status ping {0}ms'.format(latency))
+            embed.set_author(name='MPSVR Status')
             embed.set_footer(text='End of list')
 
             await ctx.send(embed=embed)
-        else:
+
+
+        except:
             embed = discord.Embed(
-            title=('The Server is Offline'),
+            title=('MPSVR is Offline'),
             description=('Yell at <@166311283744964608>!')
             )
             await ctx.send(embed=embed)
