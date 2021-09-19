d628d2a0411bc8e4c729eb7f01f31338b10a3d83
ExpertNugget <73802442+ExpertNugget@users.noreply.github.com>
2021-09-19 10:02:46 -0600

    Update mycog.py

--- mps/mycog.py
+++ mps/mycog.py
@@ -27,7 +27,6 @@ class MPS(commands.Cog):
                        .format(status.players.online)),
                 description=('{0}'.format(", ".join(query.players.names)))
             )
-            embed.set_image(url="https://premium.bisecthosting.com/index.php?r=status/95613.png")
             embed.set_author(name='MPSMC Status')
             embed.set_footer(text='End of list')
 
